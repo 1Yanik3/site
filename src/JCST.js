596e9a74cc8b8cad51f7de69977014f3de8a73ba
Yanik Ammann <confused@ant.lgbt>
2021-07-01 20:53:07 +0200

    refactoring

---
+++ src/JCST.js
@@ -0,0 +1,10 @@
+// CHANGE THIS!!!!
+const hoursOffset = 1;
+
+const GetClock = element => {
+    element.innerText = moment().utcOffset(hoursOffset).format('dddd, Do MMMM YYYY, h:mm:ss A');
+    setTimeout(() => GetClock(element), 1000)
+}
+
+document.querySelector('#clockboxOffset').innerText = (hoursOffset >= 0) ? `+${hoursOffset}` : hoursOffset;
+GetClock(document.getElementById('clockbox'))
